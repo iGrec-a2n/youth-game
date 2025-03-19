534912bc739f88989bb7c693271aa3ff6570aeaa
Lm-hg <you@example.com>
2025-03-19 12:36:38 +0100

    base de donnees en ligne

--- backend/bd.py
+++ backend/bd.py
@@ -1,122 +1,104 @@
 from pymongo import MongoClient
-# import requests
+import requests
 
-# server = "34.251.25.31" # or localhost
-# username = "ortecusdb"
-# password = "passdb*2019#"
-# port = 27017
+server = "34.251.25.31" # or localhost
+username = "ortecusdb"
+password = "passdb*2019#"
+port = 27017
 
-# if server == 'localhost':
-#     client = MongoClient("mongodb://localhost", username=username,password=password, port=port)
-# else:
-#     client = MongoClient(server, username=username,password=password, port=port)
+if server == 'localhost':
+    client = MongoClient("mongodb://localhost", username=username,password=password, port=port)
+else:
+    client = MongoClient(server, username=username,password=password, port=port)
 # creation DB
 client = MongoClient("localhost:27017")
-db = client["HACKATON"]
+db = client["HACKATON_Hetic"]
+# users = db["Users"]
+# user_scores = db["Score"]
+# # creation collection
+# db["rooms"].insert_one(
+#   {
 
-# creation collection
-db.questions.insert_many([
-  {
-    "type": "oddOneOut",
-    "question": "Quel est l'intrus parmi ces océans ?",
-    "options": ["Océan Atlantique", "Océan Pacifique", "Océan Indien", "Mer Méditerranée"],
-    "correctAnswer": "Mer Méditerranée",
-    "points": 10
-  },
-  {
-    "type": "textInput",
-    "question": "Quel est le premier pays à avoir lancé un homme dans l'espace ?",
-    "correctAnswer": "Union Soviétique",
-    "points": 5
-  },
-  {
-    "type": "multipleChoice",
-    "question": "Quel est le pays avec la plus grande population ?",
-    "options": ["Inde", "Chine", "États-Unis", "Indonésie"],
-    "correctAnswer": "Chine",
-    "points": 7
-  },
-  {
-    "type": "trueFalse",
-    "question": "Les tortues sont des reptiles.",
-    "options": ["Vrai", "Faux"],
-    "correctAnswer": "Vrai",
-    "points": 3
-  },
-  {
-    "type": "oddOneOut",
-    "question": "Quel est l'intrus parmi ces animaux ?",
-    "options": ["Aigle", "Vautour", "Hirondelle", "Condor"],
-    "correctAnswer": "Hirondelle",
-    "points": 8
-  },
-  {
-    "type": "textInput",
-    "question": "Quel est le nom de la plus grande planète de notre système solaire ?",
-    "correctAnswer": "Jupiter",
-    "points": 6
-  },
-  {
-    "type": "multipleChoice",
-    "question": "Quel est l'élément chimique dont le symbole est 'H' ?",
-    "options": ["Hydrogène", "Hélium", "Hafnium", "Holmium"],
-    "correctAnswer": "Hydrogène",
-    "points": 5
-  },
-  {
-    "type": "trueFalse",
-    "question": "Le Panama est situé en Asie.",
-    "options": ["Vrai", "Faux"],
-    "correctAnswer": "Faux",
-    "points": 4
-  },
-  {
-    "type": "oddOneOut",
-    "question": "Quel est l'intrus parmi ces instruments de musique ?",
-    "options": ["Guitare", "Piano", "Batterie", "Flûte", "Cymbale"],
-    "correctAnswer": "Cymbale",
-    "points": 9
-  },
-  {
-    "type": "textInput",
-    "question": "Quel est le premier film à avoir remporté l'Oscar du meilleur film ?",
-    "correctAnswer": "Wings",
-    "points": 10
-  },
-  {
-    "type": "multipleChoice",
-    "question": "Dans quelle ville se trouve la Statue de la Liberté ?",
-    "options": ["Chicago", "Los Angeles", "New York", "Washington D.C."],
-    "correctAnswer": "New York",
-    "points": 6
-  },
-  {
-    "type": "trueFalse",
-    "question": "Les kangourous sont originaires d'Afrique.",
-    "options": ["Vrai", "Faux"],
-    "correctAnswer": "Faux",
-    "points": 4
-  },
-  {
-    "type": "oddOneOut",
-    "question": "Quel est l'intrus parmi ces grandes découvertes scientifiques ?",
-    "options": ["Relativité restreinte", "Théorie de la gravitation universelle", "Théorie de l'évolution", "Électricité"],
-    "correctAnswer": "Électricité",
-    "points": 8
-  },
-  {
-    "type": "textInput",
-    "question": "Quel est le nom du célèbre scientifique qui a découvert la radioactivité ?",
-    "correctAnswer": "Marie Curie",
-    "points": 7
-  }
-]
+#   "admin_id": "1234567890",
+#   "room_code": "VGQZCN",
+#   "questions": [
+#     {
+#       "question": "TEST",
+#       "question_id": "qanm6kdi",
+#       "type": "QCM",
+#       "options": [
+#         "A",
+#         "B",
+#         "C",
+#         "D"
+#       ],
+#       "correct_answer": "A",
+#       "points": 34
+#     },
+#     {
+#       "question": "TEST2",
+#       "question_id": "qb3gxf6k",
+#       "type": "QCM",
+#       "options": [
+#         "E",
+#         "F",
+#         "G",
+#         "H"
+#       ],
+#       "correct_answer": "G",
+#       "points": 10
+#     }
+#   ],
+#   "statut": "Wait",
+#   "players": [],
+#   "status": "In progress"
+# }
+# )
+# print('fait')
+# db.questions.insert_many(
+# [
+#   {
+#     "type": "multipleChoice",
+#     "question": "Quel pays a récemment demandé à rejoindre l'Union Européenne en 2023 ?",
+#     "options": ["Ukraine", "Turquie", "Serbie", "Norvège"],
+#     "correctAnswer": "Ukraine",
+#     "points": 8
+#   },
+#   {
+#     "type": "multipleChoice",
+#     "question": "Quel est le pays le plus récent à avoir rejoint l'Union Européenne ?",
+#     "options": ["Croatie", "Roumanie", "Bulgarie", "Estonie"],
+#     "correctAnswer": "Croatie",
+#     "points": 7
+#   },
+#   {
+#     "type": "multipleChoice",
+#     "question": "Quelle institution de l'Union Européenne est responsable de la législation ?",
+#     "options": ["Le Conseil de l'Union Européenne", "Le Parlement Européen", "La Commission Européenne", "La Banque Centrale Européenne"],
+#     "correctAnswer": "Le Parlement Européen",
+#     "points": 6
+#   },
+#   {
+#     "type": "multipleChoice",
+#     "question": "Quel pays a voté pour quitter l'Union Européenne lors du référendum de 2016 ?",
+#     "options": ["Irlande", "Espagne", "Royaume-Uni", "France"],
+#     "correctAnswer": "Royaume-Uni",
+#     "points": 9
+#   },
+#   {
+#     "type": "multipleChoice",
+#     "question": "En quelle année a été signé le traité de Rome, créant la Communauté économique européenne (CEE) ?",
+#     "options": ["1957", "1961", "1973", "1985"],
+#     "correctAnswer": "1957",
+#     "points": 7
+#   }
+# ]
 
-)
-print("C'est fait")
-# qcm = db.Questions.find({"type":"multipleChoice"})
-# for i in qcm:
-#   print(i)
-# exist_user = db.Users.find_one({"email": "a@gmail.com"})
-# user_id = str(exist_user["_id"])
-# print(user_id)
+# )
+# print("C'est fait")
+# # qcm = db.Questions.find({"type":"multipleChoice"})
+# # for i in qcm:
+# #   print(i)
+# # exist_user = db.Users.find_one({"email": "a@gmail.com"})
+# # user_id = str(exist_user["_id"])
+# # print(user_id)
