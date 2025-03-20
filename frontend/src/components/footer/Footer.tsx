7609061a51754e4b25f1ac28bb63fea45449066e
Antoine <mahassadijoseph@gmail.com>
2025-03-20 01:06:03 +0100

    Add footer component with navigation links and include Material Symbols font in index.html

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,11 +1,25 @@
 import React from'react'
+import { NavLink } from 'react-router-dom'
+import "./Footer.scss"
+import home from "../../assets/icons/home.svg"
+import menu from "../../assets/icons/menu.svg"
+import rank from "../../assets/icons/rank.svg"
+import user from "../../assets/icons/user.svg"
 
 export const Footer: React.FC=()=>{
+
+  //TODO: add a link to the another page
   return(
-    <>
-    <h1>
-      ceci est le footer 
-    </h1>
-    </>
+    <div className="footer">
+      <div className="footer-content">
+        <ul className="footer-links">
+          <li className="footer-link"><NavLink to="/Quiz"><img className="footer-link-icon" src={home} alt="home" /></NavLink></li>
+          <li className="footer-link"><NavLink to="/"><img className="footer-link-icon" src={menu} alt="menu" /></NavLink></li>
+          <li className="footer-link"><NavLink to="/"><img className="footer-link-icon" src={rank} alt="rank" /></NavLink></li>
+          <li className="footer-link"><NavLink to="/"><img className="footer-link-icon" src={user} alt="user" /></NavLink></li>
+        </ul>
+      </div>
+        
+      </div>
   )
 }
